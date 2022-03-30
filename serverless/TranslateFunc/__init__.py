6fd9670c7562cd49a17e8a3e4ff841456a85b802
sadadpasa <sadadpasa@gmail.com>
2022-03-30 12:10:01 +0700

    request body

--- serverless/TranslateFunc/__init__.py
+++ serverless/TranslateFunc/__init__.py
@@ -6,19 +6,19 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        text = req_body.get('text')
+        langFrom = req_body.get('langFrom')
+        langTo = req_body.get('langTo')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if text and langFrom and langTo :
+        return func.HttpResponse(f"this is the result")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a text, langFrom, langTo in the request body to get the result",
              status_code=200
         )
